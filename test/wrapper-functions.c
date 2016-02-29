9c8bcb7ec48f129735da95cab19035fdf45ffa46
ben <benjohnwie@gmail.com>
2016-02-29 17:00:57 +0100

    Add wrapper for api_run and switch to (de-)activate wrapping

--- test/wrapper-functions.c
+++ test/wrapper-functions.c
@@ -3,6 +3,17 @@
 #include <msgpack/object.h>
 #include "rpc/sb-rpc.h"
 #include "helper-unix.h"
+#include "wrappers.h"
+#include "sb-common.h"
+
+/* enable/disable wrapper for api_run */
+bool WRAP_API_RUN = false;
+/* Declare original api_run (__real_api_run) */
+struct message_params_object * __real_api_run(string pluginlongtermpk,
+    string function_name, uint64_t callid, struct message_params_object args,
+                                              struct api_error *api_error);
+
+
 
 int __wrap_outputstream_write(UNUSED(outputstream *ostream), char *buffer, size_t len)
 {
@@ -40,4 +51,14 @@ struct callinfo *__wrap_connection_wait_for_response(UNUSED(struct connection *c
   cinfo->response = response;
 
   return (cinfo);
+}
+
+struct message_params_object * __wrap_api_run(string pluginlongtermpk,
+    string function_name, uint64_t callid, struct message_params_object args,
+                                              struct api_error *api_error){
+  if(WRAP_API_RUN){
+          return (struct message_params_object *) mock();
+  }
+
+  return __real_api_run(pluginlongtermpk, function_name, callid, args, api_error);
 }
---
+++ test/wrappers.h
@@ -0,0 +1,3 @@
+/* Switches to (de-)activate wrapping of individual functions  */
+extern bool WRAP_API_RUN;
+/* ----------------------------------------------------------- */
