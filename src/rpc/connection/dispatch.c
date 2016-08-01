8c853872fe2ba9f7a0537ac10c9c7f48be61e186
mkind <github@mknd.net>
2016-08-01 13:42:16 +0200

    verify length and pass it to decoder

--- src/rpc/connection/dispatch.c
+++ src/rpc/connection/dispatch.c
@@ -197,13 +197,14 @@ int handle_run(connection_request_event_info *info)
     return (-1);
   }
 
-  if (!meta->obj[0].data.string.str) {
+  if (!meta->obj[0].data.string.str || meta->obj[0].data.string.length != 16) {
     error_set(api_error, API_ERROR_TYPE_VALIDATION,
         "Error dispatching run API request. Invalid meta params size");
     return (-1);
   }
 
-  if (0 > base16_decode(targetpluginkey, 8, meta->obj[0].data.string)) {
+  if (0 > base16_decode(targetpluginkey, 8, meta->obj[0].data.string.str,
+    meta->obj[0].data.string.length)) {
     error_set(api_error, API_ERROR_TYPE_VALIDATION,
         "Error dispatching run API request. Invalid target plugin key");
     return (-1);
