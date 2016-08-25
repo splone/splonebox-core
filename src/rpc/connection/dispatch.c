a33d81e199fcabede8c8c195287eebe4a39139da
mkind <github@mknd.net>
2016-08-25 15:52:23 +0200

    Associated the targetpluginkey with the callid. Latter acts as key. So this actually correct.

--- src/rpc/connection/dispatch.c
+++ src/rpc/connection/dispatch.c
@@ -236,7 +236,6 @@ int handle_run(connection_request_event_info *info)
 
   args_object = request->params.obj[2];
   callid = (uint64_t) randommod(281474976710656LL);
-  //TODO is the targetpluginkey the correct one?
   hashmap_put(uint64_t, ptr_t)(callids, callid, targetpluginkey);
 
   if (api_run(targetpluginkey, function_name, callid, args_object, info->con,
